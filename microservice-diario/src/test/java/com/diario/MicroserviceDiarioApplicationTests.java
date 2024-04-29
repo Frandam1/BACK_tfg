a39f52c4edecf93ae126224630c3e6e4e3affe9b
Frandam1 <franciscomoreno.dev@gmail.com>
2024-04-29 20:49:26 +0200

    added micros config ini

---
+++ microservice-diario/src/test/java/com/diario/MicroserviceDiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
