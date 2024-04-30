e2798f699745b3dfaf48a1010207c295b0fbd502
Francisco Jose Moreno Gabaldon <fmorega@mercadona.es>
2024-04-30 15:37:54 +0200

    auth

--- microservice-diario/src/main/resources/import.sql
+++ microservice-diario/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO diario (entrada, contenido, usuario_Id) VALUES ('Juan', 'Perez', 1);
 INSERT INTO diario (entrada, contenido, usuario_Id) VALUES ('hola', 'Perez', 2);
 INSERT INTO diario (entrada, contenido, usuario_Id) VALUES ('hola23', 'Perez', 3);
+INSERT INTO diario (entrada, contenido, usuario_Id) VALUES ('prueba', 'Perez', 3);
